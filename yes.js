fddc79ebb22d94a7360c7938c3c794c1ea5d2c76
spam80 <45275318+spam80@users.noreply.github.com>
2019-02-19 13:33:55 +0100

    Update yes.js

--- yes.js
+++ yes.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js'),
     client = new Discord.Client({sisableEveryone: true})
     console.log("دوز دوز,");
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("533615836993159168");
+const mohamed= member.guild.channels.get("545651706893959221");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`**.#Bienvenue Sur Vast :violin: **`), 4000)        
